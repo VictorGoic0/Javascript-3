cab83eff29a09e103048aab55faaf7d192cef170
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-01-23 17:38:26 -0500

    code examples added to this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,9 +1,9 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. Window Binding - This will always try to bind to the window.
-* 2. Implicit Binding - When used inside of an object, this implicitly refers to the object it resides in.
-* 3. New Binding - When the new keyword is used, this is referring to the new object being created.
+* 1. Window Binding - "This" will always try to bind to the window.
+* 2. Implicit Binding - When used inside of an object, "this" implicitly refers to the object it resides in.
+* 3. New Binding - When the new keyword is used, "this" is referring to the constructor function.
 * 4. Explicit Binding - When .call, .apply, or .bind are used, this is explicitly referring to the object being passed as an argument.
 *
 * write out a code example of each explanation above
@@ -12,15 +12,26 @@
 // Principle 1
 
 // code example for Window Binding
-
+console.log(this);
 // Principle 2
 
 // code example for Implicit Binding
-
+const cindy = {
+  name: 'Cindy',
+  breed: 'Shih-tzu',
+  beg: function() {
+    return `${this.name} the ${this.breed} is begging for food.`
+  }
+}
 // Principle 3
 
 // code example for New Binding
-
+function Dog(name, breed, age) {
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+const coquito = new Dog('Coquito', 'Pomberanian', 10);
 // Principle 4
 
 // code example for Explicit Binding
