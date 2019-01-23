ff1419549cc398a30ee5c13318d53fdcdf030e29
VictorGoic0 <vgoico000@citymail.cuny.edu>
2019-01-23 16:36:40 -0500

    explanation for the four principles of this added

--- assignments/this.js
+++ assignments/this.js
@@ -1,10 +1,10 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Window Binding - This will always try to bind to the window.
+* 2. Implicit Binding - When used inside of an object, this implicitly refers to the object it resides in.
+* 3. New Binding - When the new keyword is used, this is referring to the new object being created.
+* 4. Explicit Binding - When .call, .apply, or .bind are used, this is explicitly referring to the object being passed as an argument.
 *
 * write out a code example of each explanation above
 */
